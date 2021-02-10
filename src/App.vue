3924686eb5d9ff408820d6938acdbf75e0d03488
Rizwan Rabbani <rabbani_rizwan@yahoo.com>
2021-02-10 18:32:16 +0500

    Vue.js: Building an Interface

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,124 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-center">
+      <add-appointment @add="addItem" />
+      <search-appointments
+        @searchRecords="searchAppointment"
+        @requestKey="changeKey"
+        @requestDir="changeDir"
+        :myKey="filterKey"
+        :myDir="filterDir"
+      />
+      <appointment-list
+        :appointments="filteredApt"
+        @remove="removeItem"
+        @edit="editItem"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from "axios";
+import AppointmentList from "./components/AppointmentList.vue";
+import AddAppointment from "./components/AddAppointment.vue";
+import SearchAppointments from "./components/SearchAppointment";
 
+import _ from "lodash";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // FontAwesomeIcon,
+    AppointmentList,
+    AddAppointment,
+    SearchAppointments,
+  },
+  computed: {
+    searchedApt: function() {
+      return this.appointments.filter((item) => {
+        return (
+          item.petName.toLowerCase().match(this.searchTerm.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerm.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerm.toLowerCase())
+        );
+      });
+    },
+    filteredApt: function() {
+      return this.searchedApt;
+    },
+  },
+  watch: {
+    filteredApt: function() {
+      this.searchedApt.sort(this.compared);
+    },
+  },
+  data: function() {
+    return {
+      title: "Appointment List",
+      appointments: [],
+      aptIndex: 0,
+      searchTerm: "",
+      filterKey: "petOwner",
+      filterDir: "desc",
+    };
+  },
+  mounted: function() {
+    axios.get("./data/appointments.json").then(
+      (response) =>
+        (this.appointments = response.data.map((item) => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item;
+        }))
+    );
+  },
+  methods: {
+    compared: function(a, b) {
+      //console.log(a[this.filterDir])
+      if (this.filterDir == "asc") {
+        if (a[this.filterKey] < b[this.filterKey]) {
+          return -1;
+        }
+        if (a[this.filterKey] > b[this.filterKey]) {
+          return 1;
+        }
+      } else {
+        if (a[this.filterKey] < b[this.filterKey]) {
+          return 1;
+        }
+        if (a[this.filterKey] > b[this.filterKey]) {
+          return -1;
+        }
+      }
+
+      return 0;
+    },
+    changeKey: function(val) {
+      this.filterKey = val;
+      this.appointments.sort(this.compared);
+    },
+    changeDir: function(val) {
+      this.filterDir = val;
+      this.appointments.sort(this.compared);
+    },
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, { aptId: id });
+      this.appointments[aptIndex][field] = text;
+    },
+    addItem: function(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    },
+    searchAppointment: function(term) {
+      this.searchTerm = term;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/AppointmentList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="col-12 col-md-10 col-lg-7">
+    <div class="list-group list-group-flush">
+      <div
+        class="list-group-item d-flex align-items-start"
+        v-for="item in appointments"
+        :key="item.aptIndex"
+      >
+        <button
+          @click="$emit('remove', item)"
+          class="mr-2 btn btn-sm btn-danger"
+        >
+          <font-awesome-icon icon="trash" />
+        </button>
+        <div class="w-100">
+          <div class="d-flex justify-content-between">
+            <span
+              class="h4 text-primary"
+              contenteditable="true"
+              @blur="
+                $emit('edit', item.aptId, 'petName', $event.target.innerText)
+              "
+              >{{ item.petName }}</span
+            >
+            <span class="float-right">{{ formattedDate(item.aptDate) }}</span>
+          </div>
+          <div class="owner-name">
+            <span class="font-weight-bold text-primary mr-1">Owner:</span>
+            <span
+              contenteditable="true"
+              @blur="
+                $emit('edit', item.aptId, 'petOwner', $event.target.innerText)
+              "
+              >{{ item.petOwner }}</span
+            >
+          </div>
+          <div
+            contenteditable="true"
+            @blur="
+              $emit('edit', item.aptId, 'aptNotes', $event.target.innerText)
+            "
+          >
+            {{ item.aptNotes }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import moment from "moment";
+export default {
+  name: "AppointmentList",
+  props: ["appointments"],
+  components: {
+    FontAwesomeIcon,
+  },
+  methods: {
+    formattedDate: function(date) {
+      return moment(new Date(date)).format('MMMM Do YYYY, h:mm a');
+    },
+  },
+};
+</script>
